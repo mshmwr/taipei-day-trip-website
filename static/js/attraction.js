51d6d09f2edeba4817412f4914c7e4ee4fa78ab7
DESKTOP-COIOO24\user <mshmwr20@gmail.com>
2021-05-13 23:36:29 +0800

    clean up the code

--- static/js/attraction.js
+++ static/js/attraction.js
@@ -1,10 +1,3 @@
-//import js file
-let newscript = document.createElement("script");
-newscript.setAttribute("type", "text/javascript");
-newscript.setAttribute("src", "../static/js/dataManager.js");
-let head = document.getElementsByTagName("head")[0];
-head.appendChild(newscript);
-
 let attModels = {
   data: null,
   parsedData: null,
