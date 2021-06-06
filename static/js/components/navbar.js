96ee97c2ab423ba95358c74479da428cc155fc66
DESKTOP-COIOO24\user <mshmwr20@gmail.com>
2021-06-06 13:44:29 +0800

    refactor: modify footer's css

--- static/js/components/navbar.js
+++ static/js/components/navbar.js
@@ -1,10 +1,10 @@
 navbar =
   '    <nav>\
 <div class="navDiv">\
-  <div class="common-text navDiv-logo" id="navDiv-logo">台北一日遊</div>\
-  <div class="navDiv-menu">\
-    <div class="common-text navDiv-menu-btn-text" id="navBooking">預定行程</div>\
-    <div class="common-text navDiv-menu-btn-text" id="userState">登入/註冊</div>\
+  <div class="common-text navDiv--logo" id="navDiv-logo">台北一日遊</div>\
+  <div class="navDiv--menu">\
+    <div class="common-text navDiv--menu--btn-text" id="navBooking">預定行程</div>\
+    <div class="common-text navDiv--menu--btn-text" id="userState">登入/註冊</div>\
   </div>\
 </div>\
 </nav>\
