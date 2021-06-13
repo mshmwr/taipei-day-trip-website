3c9e49863ce8849d20f454b6694bd8fed27828c5
DESKTOP-COIOO24\user <mshmwr20@gmail.com>
2021-06-13 18:03:13 +0800

    refactor:modify dialog.css by using BEM design pattern, and unify the font property, and add hover effect

--- static/js/components/dialog.js
+++ static/js/components/dialog.js
@@ -2,43 +2,41 @@ dialog =
   '      <div class="dialog" id="dialog">\
   <div class="dialogMask" id="dialogMask"></div>\
   <div class="dialogBox">\
-    <div class="dialogBox-decoratorBar"></div>\
-    <div class="dialogBox-icon" id="closeIcon">\
+    <div class="dialogBox__decoratorBar"></div>\
+    <div class="dialogBox__icon" id="closeIcon">\
       <img />\
     </div>\
-    <div class="dialogBox-main">\
+    <div class="dialogBox__form">\
       <form id="dialogForm">\
-        <p class="dialogBox-mainText dialogContent">登入會員帳號</p>\
+        <p class="common-text common-text-bold dialogBox__form-text dialogContent">登入會員帳號</p>\
         <input\
           type="text"\
           placeholder="輸入姓名"\
-          class="dialogContent dialogBox__form__input"\
+          class="common-text dialogContent dialogBox__form__inputField"\
           required\
         />\
         <input\
           type="email"\
           placeholder="輸入電子信箱"\
           id="dialog-email"\
-          class="dialogContent dialogBox__form__input"\
+          class="dialogContent dialogBox__form__inputField"\
           required\
         />\
         <input\
           type="password"\
           placeholder="輸入密碼"\
-          class="dialogContent dialogBox__form__input"\
+          class="dialogContent dialogBox__form__inputField"\
           autocomplete="on"\
           required\
         />\
         <input\
           type="submit"\
           value="登入帳戶"\
-          class="dialogContent dialogBox__form__btn"\
+          class="common__btn dialogContent dialogBox__form__btn"\
         />\
       </form>\
-      <p class="dialogMessage" id="dialogMessage">error msg</p>\
-      <div class="dialogLoginRegister">\
-        <a href="#" class="dialogContent">還沒有帳戶？點此註冊</a>\
-      </div>\
+      <p class="dialogBox__form__message" id="dialogMessage">error msg</p>\
+      <a href="#" class="common-text dialogContent dialogBox__form__loginRegisterText">還沒有帳戶？點此註冊</a>\
     </div>\
   </div>\
 </div>\
